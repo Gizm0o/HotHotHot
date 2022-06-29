2e3317fe9b39a6d3e8f1d21a8fdab2200d5b167c
Gwendal Besnard <gwendal.besnard@etu.univ-amu.fr>
2022-06-29 19:57:21 +0200

    new function

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import UpdateObserver from "./scripts/Update.js";
 import NotificationObserver from "./scripts/Notifications.js";
 
 
-// //Subjects 
-// const SubjectSensors = new SubjectSensor();
+//Subjects 
+const SubjectSensors = new SubjectSensor();
 
-// //Observers
-// const UpdateObservers = new UpdateObserver();
-// const NotificationObservers = new NotificationObserver();
+//Observers
+const UpdateObservers = new UpdateObserver();
+const NotificationObservers = new NotificationObserver();
 
 
 //Binding
-SubjectSensor.addObserver(UpdateObserver);
-SubjectSensor.addObserver(NotificationObserver);
+SubjectSensors.attach(UpdateObservers);
+SubjectSensors.attach(NotificationObservers);
