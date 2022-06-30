41796dde2a07fa0071af0876e2552d52162318ff
Gizmo <leon.loncaric@etu.univ-amu.fr>
2022-06-30 22:29:50 +0200

    merge conflict fix

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ import SubjectSensor from "./scripts/sensor.js";
 import UpdateObserver from "./scripts/Update.js";
 import NotificationObserver from "./scripts/Notifications.js";
 import Historic from "./scripts/historic.js";
-<<<<<<< HEAD
-
-=======
->>>>>>> 80e63d7a712f709da45fc487e707274656891793
 
 
 //Subjects 
