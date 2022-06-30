fd0c0f43289586eccdcf737c45227eab1f6c51ef
Gizmo <leon.loncaric@etu.univ-amu.fr>
2022-06-30 22:17:05 +0200

    added imports

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import SubjectSensor from "./scripts/sensor.js";
 import UpdateObserver from "./scripts/Update.js";
 import NotificationObserver from "./scripts/Notifications.js";
+import Historic from "./scripts/historic.js";
+import chart from "./scripts/chart.js";
 
 
 //Subjects 
@@ -9,8 +11,13 @@ const SubjectSensors = new SubjectSensor();
 //Observers
 const UpdateObservers = new UpdateObserver();
 const NotificationObservers = new NotificationObserver();
+const HistoricObservers = new Historic();
 
 
 //Binding
 SubjectSensors.attach(UpdateObservers);
 SubjectSensors.attach(NotificationObservers);
+SubjectSensor.attach(HistoricObservers);
+
+
+chart();
