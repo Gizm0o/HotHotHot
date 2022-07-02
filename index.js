477839ea4fc0adbb82862f49bab8663d74f07b90
Gizmo <leon.loncaric@etu.univ-amu.fr>
2022-07-02 22:32:56 +0200

    fix : forgot imports in index

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-import SubjectSensor from "./scripts/sensor.js";
+import SensorSubject from "./scripts/Sensor.js";
 import UpdateObserver from "./scripts/Update.js";
 import NotificationObserver from "./scripts/Notifications.js";
-import Historic from "./scripts/historic.js";
+import Historic from "./scripts/Historic.js";
+import NavBarSubject from "./scripts/NavBar.js";
+import SensorSubject from "./scripts/Sensor.js";
 
 
 //Subjects 
-const SubjectSensors = new SubjectSensor();
+const SensorSubject = new SensorSubject();
+const NavBarSubject = new NavBarSubject();
 
 //Observers
 const UpdateObservers = new UpdateObserver();
 const NotificationObservers = new NotificationObserver();
 const HistoricObservers = new Historic();
+const NavigationObserver = new NavigationObserver();
 
 
 //Binding
-SubjectSensors.attach(UpdateObservers);
-SubjectSensors.attach(NotificationObservers);
-SubjectSensors.attach(HistoricObservers);
+SensorSubject.attach(UpdateObservers);
+SensorSubject.attach(NotificationObservers);
+SensorSubject.attach(HistoricObservers);
+
+NavBarSubject.attach(NavigationObserver);
 
