3874b711e767b299a31f3f9f50584d731c514320
Gizmo <leon.loncaric@etu.univ-amu.fr>
2022-07-02 23:06:27 +0200

    last fix maybe

--- scripts/Navigation.js
+++ scripts/Navigation.js
@@ -3,6 +3,7 @@ import Observer from './Observer';
 class NavigationObserver extends Observer {
 
     update() {
+        console.log('NavigationObserver');
         document.querySelectorAll("#main section").forEach(section => 
             section.setAttribute("style", "display: none;"));
         document.querySelector("#main #${data.state}")
