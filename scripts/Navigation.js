c93e7367bd3a061baabddcd151bba091f88ec3e4
Gizmo <leon.loncaric@etu.univ-amu.fr>
2022-07-02 20:51:00 +0200

    fix

--- scripts/Navigation.js
+++ scripts/Navigation.js
@@ -3,11 +3,11 @@ import Observer from './Observer';
 class NavigationObserver extends Observer {
 
     update() {
-        document.querySelectorAll("#main section").forEach(section => {
-            section.removeAttribute("class");
-        document.querySelector("#main #${data.state}").setAttribute("class", "active");
+        document.querySelectorAll("#main section").forEach(section => 
+            section.removeAttribute("class"));
+        document.querySelector("#main #${data.state}")
+            .setAttribute("class", "active");
         }
     }
-}
 
 export default NavigationObserver;
