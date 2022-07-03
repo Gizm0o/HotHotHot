baa3fafafebe9db238543fe7f5c41043b77a96aa
Gizmo <leon.loncaric@etu.univ-amu.fr>
2022-07-03 10:28:27 +0200

    reworked update + refactoring

--- scripts/Update.js
+++ scripts/Update.js
@@ -2,57 +2,36 @@ import Observer from './Observer.js';
 class UpdateObserver extends Observer {
     constructor() {
         super();
+
+        this.ExtTemp = document.getElementById('dataExt');
+        this.IntTemp = document.getElementById('dataInt');
+        if(localStorage.getItem('Temp') != null) {
+            this.getLocalData();
+        }
+    }
+
+    saveDataLocal(ExtTemp, IntTemp) {
+        const data = {
+            ExtTemp: ExtTemp,
+            IntTemp: IntTemp
+        }
+        localStorage.setItem('Temp', JSON.stringify(data));
+    }
+
+    getLocalData() {
+        let data = JSON.parse(localStorage.getItem('Temp'));
+        this.ExtTemp.innerText = data.ExtTemp;
+        this.IntTemp.innerText = data.IntTemp;
     }
 
     update(data) {
         console.log(data);
 
-        var A_temperatures = [];
-        var Name_temperatures = [];
-        var T_temperatures = [];
-
-        var object2 = data;
-        
-        for (var [key, value] of Object.entries(object2)) {
-                console.log(value);
-                if(key == "state"){
-                    for (var [key2, value2] of Object.entries(value)) {
-                        console.log(`${key2}: ${value2.Valeur}`);
-                        A_temperatures.push(value2.Valeur);
-                        Name_temperatures.push(value2.Nom);
-                        T_temperatures.push(value2.type);
-                    }
-                }
-            } 
-
-        console.log(A_temperatures);
-        console.log(Name_temperatures);
-        console.log(T_temperatures);
-
-        var i = 0;
-
-        var interval = setInterval(function() {
-            if(i < A_temperatures.length) {
-                if(document.getElementById('titre_message'))
-                    document.getElementById('titre_message').remove();
-                    
-                let I_temperature = A_temperatures[i]; 
-                let IName_temperature = Name_temperatures[i];               
-                ++i;
-
-                if (IName_temperature == 'interieur') {
-                    var temp = document.getElementById('dataInt').innerText = I_temperature;
-                }else {
-                    var temp = document.getElementById('dataExt').innerText = I_temperature;
-                }
-                console.log(temp);
-
-            } 
-            else {
-                clearInterval(interval);
-                interval = null;
-            }
-        }, 2000)
+        const ExtTemp = this.ExtTemp.innerText;
+        const IntTemp = this.IntTemp.innerText;
+        this.ExtTemp.innerText = data.ExtTemp;
+        this.IntTemp.innerText = data.IntTemp;
+        this.saveDataLocal(data.ExtTemp, data.IntTemp);
     }
 }
 
