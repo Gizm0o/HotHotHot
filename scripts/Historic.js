5691de39efb814a72c913e1393d39d7caad44714
Gwendal Besnard <gwendal.besnard@etu.univ-amu.fr>
2022-07-03 22:56:05 +0200

    reinject historic js

--- scripts/Historic.js
+++ scripts/Historic.js
@@ -1,68 +1,68 @@
-// import Observer from "./Observer.js";
+import Observer from "./Observer.js";
 
-// class HistoricObserver extends Observer {
-//     _callback
+class HistoricObserver extends Observer {
+    _callback
 
-//     set callback(value) {
-//         this._callback = value;
-//     }
+    set callback(value) {
+        this._callback = value;
+    }
 
-//     constructor() {
-//         super();
-//         this.historicTableBody = document.querySelector('tbody');
-//         this.historicTableTemplate = document.querySelector('#historicTableTemplate');
-//         if ( localStorage.getItem('historic') !== null ) {
-//             this.getLocalData();
-//         }
-//     }
+    constructor() {
+        super();
+        this.historicTableBody = document.querySelector('tbody');
+        this.historicTableTemplate = document.querySelector('#historicTableTemplate');
+        if ( localStorage.getItem('historic') !== null ) {
+            this.getLocalData();
+        }
+    }
 
-//     addRow(sensor) {
-//         let clonedRow = document.importNode(this.historicTableTemplate.content, true);
-//         let cells = clonedRow.querySelectorAll('td');
-//         cells[0].innerText = sensor.Nom;
-//         cells[1].innerText = sensor.Valeur + '°C';
-//         cells[2].innerText = sensor.Type;
-//         cells[3].innerText = sensor.Date ? sensor.Date : new Date().toLocaleString();
-//         this.historicTableBody.appendChild(clonedRow);
-//         if (this._callback) {
-//             this._callback.notify();
-//         }
-//     }
+    addRow(sensor) {
+        let clonedRow = document.importNode(this.historicTableTemplate.content, true);
+        let cells = clonedRow.querySelectorAll('td');
+        cells[0].innerText = sensor.Nom;
+        cells[1].innerText = sensor.Valeur + '°C';
+        cells[2].innerText = sensor.Type;
+        cells[3].innerText = sensor.Date ? sensor.Date : new Date().toLocaleString();
+        this.historicTableBody.appendChild(clonedRow);
+        if (this._callback) {
+            this._callback.notify();
+        }
+    }
 
-//     saveDataLocal(data) {
-//         console.log(localStorage.getItem('historic'));
-//         console.log(data);
-//         if ( localStorage.getItem('historic') == null ) {
-//             localStorage.setItem('historic','[]');
-//         }
-//         let historic = JSON.parse(localStorage.getItem('historic'));
-//         data.forEach(element => {
-//             historic.push({
-//                 Name : element.Name,
-//                 Value : element.Value,
-//                 Date : element.Date
-//             })
-//         });
-//         localStorage.setItem('historic',JSON.stringify(historic));
-//     }
+    saveDataLocal(data) {
+        console.log(localStorage.getItem('historic'));
+        console.log(data);
+        if ( localStorage.getItem('historic') == null ) {
+            localStorage.setItem('historic','[]');
+        }
+        let historic = JSON.parse(localStorage.getItem('historic'));
+        data.forEach(element => {
+            historic.push({
+                Name : element.Name,
+                Value : element.Value,
+                Date : element.Date
+            })
+        });
+        localStorage.setItem('historic',JSON.stringify(historic));
+    }
 
-//     getLocalData() {
-//         console.log(localStorage.getItem('historic'));
-//         let historic = JSON.parse(localStorage.getItem('historic'));
-//         historic.forEach(element => {
-//             this.addRow(element);
-//         });
-//     }
+    getLocalData() {
+        console.log(localStorage.getItem('historic'));
+        let historic = JSON.parse(localStorage.getItem('historic'));
+        historic.forEach(element => {
+            this.addRow(element);
+        });
+    }
 
-//     update(data) {
-//         data.state.forEach(sensor => {
-//             sensor.Date = new Date().toLocaleString;
-//             this.addRow(sensor);
-//         });
+    update(data) {
+        data.state.forEach(sensor => {
+            sensor.Date = new Date().toLocaleString;
+            this.addRow(sensor);
+        });
 
-//         this.saveDataLocal(data.state);
-//         }
-//     }
+        this.saveDataLocal(data.state);
+        }
+    }
 
 
-// export default HistoricObserver;
+export default HistoricObserver;
